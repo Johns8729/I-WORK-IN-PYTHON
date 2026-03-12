9152c19dc22f3a0e6072fe9613053bd3decacaaf
Johns8729 <jhonsebastian8729@gmail.com>
2026-03-12 06:38:00 -0500

    Implement sales registration and total calculation
    Added functionality to register sales and calculate totals.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from calcular import calcular_total
+from mensaje import mostrar_resumen
+
+store_products = []
+
+print("...Welcome to the store...")
+
+total_of_the_day = 0
+continue_sale = "yes"
+stop = "no"
+
+while continue_sale == "yes":
+
+    product = input("Product name: ")
+    price = float(input("Product price: "))
+    quantity = int(input("Quantity sold: "))
+
+    total = calcular_total(price, quantity)
+    total_of_the_day += total
+
+    store_products.append((product, price, quantity))
+
+    print("Sale registered.\nTotal of the sale: $", total)
+
+    continue_sale = input("Do you want to make another sale? (yes/no): ").lower()
+
+    if continue_sale == stop:
+        mostrar_resumen(store_products, total_of_the_day)
 
