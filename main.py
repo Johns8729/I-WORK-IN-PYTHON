82cf5a5276798f4d95a32c9a43d9e6457f1069d2
Johns8729 <jhonsebastian8729@gmail.com>
2026-03-12 06:57:19 -0500

    Add comments for product sale calculations
    Added comments to clarify code functionality.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from calcular import calcular_total
 from mensaje import mostrar_resumen
-
+# List to store all products sold during the day
 store_products = []
 
 print("...Welcome to the store...")
@@ -14,10 +14,10 @@ while continue_sale == "yes":
     product = input("Product name: ")
     price = float(input("Product price: "))
     quantity = int(input("Quantity sold: "))
-
+# Calculate the total price of the sale
     total = calcular_total(price, quantity)
     total_of_the_day += total
-
+ # Save the product information in the list
     store_products.append((product, price, quantity))
 
     print("Sale registered.\nTotal of the sale: $", total)
