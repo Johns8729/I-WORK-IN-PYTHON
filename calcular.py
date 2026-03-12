d80fafeead4ca0e2205f937a51c424de1237311b
Johns8729 <jhonsebastian8729@gmail.com>
2026-03-12 09:53:17 -0500

    Rename parameter 'cantidad' to 'amount' in calcular_total

--- calcular.py
+++ calcular.py
@@ -1,3 +1,3 @@
 # Function that calculates the total price of a sale
-def calcular_total(precio, cantidad):
-  return precio * cantidad
+def calcular_total(precio, amount):
+  return precio * amount
